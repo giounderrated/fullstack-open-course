47f8c739d8f58f2db1ec2ef3ff8d5aad69da4b6f
giondcode <giovaniflores162@gmail.com>
2023-12-21 21:55:27 -0600

    4.18: bloglist expansion, step6: Added Token Generation for login endpoint

--- part4/bloglist/src/app.js
+++ part4/bloglist/src/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
-require('express-async-errors')
+require("express-async-errors");
 const cors = require("cors");
 const config = require("./utils/Config");
 const logger = require("./utils/Logger");
 const middleware = require("./utils/Middleware");
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const blogsRouter = require("./controllers/Blog.controller");
 const usersRouter = require("./controllers/Users.controller");
+const loginRouter = require("./controllers/Login.controller");
 const BLOGS_PATH = "/api/blogs";
-const USERS_PATH = "/api/users"
-
+const USERS_PATH = "/api/users";
+const AUTH_PATH = "/api/auth/login";
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use(middleware.requestLogger);
 
 app.use(BLOGS_PATH, blogsRouter);
 app.use(USERS_PATH, usersRouter);
-
+app.use(AUTH_PATH, loginRouter);
 
 mongoose.set("strictQuery", false);
 
@@ -38,6 +38,4 @@ mongoose
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
-module.exports = app
-
-
+module.exports = app;
---
+++ part4/bloglist/src/controllers/Login.controller.js
@@ -0,0 +1,33 @@
+const loginRouter = require("express").Router();
+const User = require("../models/User.model")
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
+
+loginRouter.post("/", async(request,response)=>{
+  const {username,password} = request.body 
+  const user = await User.findOne({username})
+  
+  const exists = user!=null
+  const passwordCorrect = await bcrypt.compare(password, user.passwordHash)
+  
+  if(!exists || !passwordCorrect){
+    return response.status(401).json({
+      error:'Invalid username or password'
+    })
+  }
+
+  const userData = {
+    id:user.id,
+    username:user.username
+  }
+
+  const token = jwt.sign(userData, process.env.SECRET)
+
+  response
+  .status(200)
+  .send({ token, username: user.username, name: user.name })
+
+})
+
+module.exports = loginRouter
+
